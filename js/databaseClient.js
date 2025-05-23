f49c31cd9a3118fb9ed9b8121b305155a34e8525
danielsimmen <165002010+danielsimme@users.noreply.github.com>
2025-05-23 13:18:16 +0200

    Formular Überarbeitung

--- js/databaseClient.js
+++ js/databaseClient.js
@@ -7,8 +7,8 @@ Lesen Sie alle Kommentare.
 */
 
 // Ändern Sie die folgenden beiden Werte, um ihre Datenbank zu verbinden.
-const GROUP_NAME = "teacher";
-const PASSWORD = "vf9bm0k4uoji7o4b";
+const GROUP_NAME = "al7";
+const PASSWORD = "j4x3y68radaf1air";
 
 const SERVER_URL = "https://ict-290.herokuapp.com/sql";
 const databaseClient = {
